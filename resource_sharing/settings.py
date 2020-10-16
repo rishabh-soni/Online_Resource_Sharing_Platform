1c387e327715221b3bbcff054dc63a5d020781d2
rishabh-soni <rishabh.rs.001@gmail.com>
2020-10-17 00:21:22 +0530

    Created Login Page

--- resource_sharing/settings.py
+++ resource_sharing/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage'
+    'homepage',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- resource_sharing/urls.py
+++ resource_sharing/urls.py
@@ -15,8 +15,11 @@ Including another URL conf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', include('homepage.urls')),
-]
+    path('login/', include('login.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
