95c132d5c99766cd22e370685c803b44cdc8d1bb
rishabh-soni <rishabh.rs.001@gmail.com>
2020-10-20 18:42:12 +0530

    Fixed an error.

--- resource_sharing/settings.py
+++ resource_sharing/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage',
     'login',
-    'accounts',
     'authen.apps.AuthenConfig',
 ]
 
