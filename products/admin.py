02a44c7618236710f1963fa7482c87951fd01b2c
saket-07 <cse190001054@iiti.ac.in>
2020-10-22 20:11:54 +0530

    changed something

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Products
 
-# Register your models here.
+class ProductsAdmin(admin.ModelAdmin):
+    list_display=('name', 'price')
+
+
+admin.site.register(Products, ProductsAdmin)
