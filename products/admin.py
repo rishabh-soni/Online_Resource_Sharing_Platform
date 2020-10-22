4ac3deeed35a25738db634a2453a50f431f72b8c
rishabh-soni <rishabh.rs.001@gmail.com>
2020-10-22 20:00:01 +0530

    Added products

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Products
 
-class ProductsAdmin(admin.ModelAdmin):
-    list_display=('name', 'price')
-
-
-admin.site.register(Products, ProductsAdmin)
+# Register your models here.
--- products/models.py
+++ products/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-class Products(models.Model):
-    name=models.CharField(max_length=255)
-    description = models.CharField(max_length=2083)
-    seller = models.CharField(max_length=255)
-    price = models.FloatField()
-    category = models.CharField(max_length=255)
-    status = models.IntegerField()
-    image_url = models.CharField(max_length=2083)
-
+# Create your models here.
