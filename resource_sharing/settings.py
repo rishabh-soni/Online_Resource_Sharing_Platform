ed3e388f244631c0bb70efbc1bff02d7b28e768e
rishabh-soni <rishabh.rs.001@gmail.com>
2020-11-16 16:35:26 +0530

    Added sqldump directory

--- resource_sharing/settings.py
+++ resource_sharing/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$oe-u6-s0$$6yv07%2cyn=f2h!y*dku)e^c%i-n&ko8z3&myp2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
